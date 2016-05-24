f289a2022849fa38d3be959000f50f3819eb3dad
Justine <chiujustine@gmail.com>
2016-05-24 12:25:34 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,187 @@
+//===================================================*/
+/* GLOBAL VARIABLES
+===================================================*/
+
+// Empty object for the ciderApp to live in
+var ciderApp = {};
+
+// LCBO API URL with Access Key
+ciderApp.apiUrl = 'https://lcboapi.com/products/?access_key=MDplMzgyZmMzNC0xZmEyLTExZTYtYTRkOS1jZmFmZTcxODU4YTI6NEhMYURFWkptTXByNlZhQXFFVUk5cGcxNTlxa3A4cE5Pc0xN';
+
+//===================================================*/
+/* COLLECT INFORMATION
+===================================================*/
+
+// When the user clicks on a selection...
+ciderApp.listeners = function(){
+
+
+// If user clicks on SWEET...
+	$('.globalTaste.sweet').on('click', function() {
+			$('.subSweetChoices').fadeIn();
+		});
+	
+	$('.subSweetChoices .beforeSelection input').on('click', function(){
+		var whatIreallyClicked 	= $(this).parent();
+		var myParentElement = whatIreallyClicked[0];
+			$('.subSweetChoices .beforeSelection').removeClass('selectionMade');
+			$(myParentElement).addClass('selectionMade');
+	});
+
+// If user clicks on DRY...
+	$('.globalTaste.dry').on('click', function() {
+			$('.subDryChoices').fadeIn();
+		});
+
+	$('.subDryChoices .beforeSelection input').on('click', function(){
+		var whatIreallyClicked 	= $(this).parent();
+		var myParentElement = whatIreallyClicked[0];
+			$('.subDryChoices .beforeSelection').removeClass('selectionMade');
+			$(myParentElement).addClass('selectionMade');
+	});
+
+
+// If user clicks on SURPRISE ME...
+	$('.surpriseMe').on('click', function() {
+			ciderApp.getData('random');
+		});
+}
+
+ciderApp.init = function(){
+
+// When the form is submitted, prevent the default refresh event
+	$('form').on('submit', function(event) {
+		event.preventDefault();
+// Store the user's choice into a variable that carries the value
+// (e.g. "Sweet & Fruity")
+		var userChoice = $('input:checked').val();
+
+// Restart thingy needs to live here
+		ciderApp.restart();
+// Get data for userChoice
+		ciderApp.getData(userChoice);
+	
+	}); // -- end of form submit -->
+
+		ciderApp.listeners();
+
+	}; // -- end of .init
+
+//===================================================*/
+/* AJAX CALL
+===================================================*/
+// Here we flesh out the previously referenced ".getData",
+// using an AJAX call
+// Getting all the ciders first - responds with an array of objects
+ciderApp.getData = function(decision){
+
+	$.ajax({
+		url: ciderApp.apiUrl,
+		method: 'GET',
+		dataType: 'json',
+		data: {
+			q: 'Ciders',
+			per_page: '100'
+		} // data
+	}) // -- $.ajax -->
+	.then(function(res){
+		console.log(res.result);
+		var onlyImages = res.result.filter(function(cider) {
+			return cider.image_url
+		});
+
+// ALTERNATE APPROACHES TO FILTER; understand how they work and why we don't use them:
+
+// var filteredResults = [];
+// res.result.forEach(function(cider) {
+// 	if (cider.style === decision || cider.style === `Medium-${decision}`) {
+// 		cider.myNewKey = 'thing';
+// 		filteredResults.push(cider);
+// 	}
+// });
+// var mappedResults = res.result.map(function(cider) {
+// 	return cider.name;
+// });
+
+//===================================================*/
+/* RANDOM RESULTS
+===================================================*/
+		if (decision === 'random'){
+			var randomResults = _.sample(onlyImages, 2);
+			ciderApp.displayCiders(randomResults);
+		} else{
+				var filteredResults = onlyImages.filter(function(cider) {
+					return cider.style === decision || cider.style === `Medium-${decision}`;
+				});
+
+				ciderApp.displayCiders(filteredResults);
+		}; // -- end of if / else statement;
+
+	}); // -- .then -->
+
+}; // -- ciderApp.getData -->
+
+
+//===================================================*/
+/* OUTPUT
+//===================================================*/
+
+ciderApp.displayCiders = function(usersCiders) {
+	$('#finalCiders').html('');
+	// usersCiders is a parameter and is required
+	// usersCiders is an array (e.g. sweetAndFruityOnly)
+	// we use forEach to go through the array
+	usersCiders.forEach(function(individualCider){
+	// individualCider is a placeholder; each item is inside the array of usersCiders
+
+	// now we dynamically create HTML elements and store them in variables
+		var ciderTitle = $('<h4>')
+			.text(individualCider.name);
+		var ciderDesc = $('<h5>')
+			.text(individualCider.tasting_note);
+		var ciderImage = $('<img>')
+			.attr('src', individualCider.image_url);
+		var ciderPrice = $('<h5>')
+			.text("$" + (individualCider.regular_price_in_cents/100).toFixed(2));
+		var ciderBuy = $('<h5 class="buy">')
+			.text("Buy");
+		var ciderDiv = $('<div class="ciderResult">')
+			.append(ciderTitle, ciderDesc, ciderImage, ciderPrice, ciderBuy);
+
+		$('#finalCiders').append(ciderDiv);
+
+		$('html, body').animate({
+	 		scrollTop: $('#here').offset().top}, 750);
+
+		}) // -- end of forEach  -->
+
+	}; // end of displayCiders
+
+
+//===================================================*/
+// RESTART THE APP
+//===================================================*/
+// When the "Again! Again!" button is pressed, do this:
+
+ciderApp.restart = function() {
+	$('.again').on('click', function(){
+		// removes previous userChoice
+		$('.subSweetChoices .beforeSelection').removeClass('selectionMade');
+		$('.subDryChoices .beforeSelection').removeClass('selectionMade');
+		// empties previous results
+		$('#finalCiders').empty();
+		// scrolls back up to top
+		$('html, body').animate({scrollTop:0},'slow');
+ 		// closes previous div opened
+		$('.subSweetChoices').fadeOut();
+		$('.subDryChoices').fadeOut();
+	});
+}
+
+//===================================================*/
+// DOCUMENT READY
+//===================================================*/
+
+$(function() {
+    ciderApp.init();
+});
